86c35562e5f784cc973a8eeba8d5f6894f5d2659
MaryamAzz <maryam.ma@hotmail.com>
2018-08-27 14:57:17 +0300

    27-8-2018 maryam

--- app/src/main/java/com/example/maryam/sproject/AddProjectFragment.java
+++ app/src/main/java/com/example/maryam/sproject/AddProjectFragment.java
@@ -33,7 +33,8 @@ public class AddProjectFragment extends Fragment implements View.OnClickListener
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_project, container, false);
+        View view= inflater.inflate(R.layout.fragment_add_project, container, false);
+        return view;
     }
 
     @Override
