76cab2475d79f590897bf08670c12305a40c6d43
MaryamAzz <maryam.ma@hotmail.com>
2018-09-22 12:55:14 +0300

    google_map

--- app/src/main/java/com/smm/sapp/sproject/Activities/MapActivity.java
+++ app/src/main/java/com/smm/sapp/sproject/Activities/MapActivity.java
@@ -1,13 +1,174 @@
 package com.smm.sapp.sproject.Activities;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MapActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.smm.sapp.sproject.R;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+    private static final float DEFAULT_ZOOM = 15f;
+    private boolean mLocationPermissionGranted = false;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        searchLocation();
+        getLocationPermission();
     }
+
+    private void searchLocation() {
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                mMap.clear();
+                mMap.addMarker(new MarkerOptions().position(place.getLatLng()).title(place.getName().toString()));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(place.getLatLng()));
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(place.getLatLng(), 12.0f));
+
+                LatLng latLng = place.getLatLng();
+                double lng = latLng.longitude;
+                double lat = latLng.latitude;
+
+                Intent returnIntent = new Intent();
+                returnIntent.putExtra("lng", lng);
+                returnIntent.putExtra("lat", lat);
+                setResult(Activity.RESULT_OK, returnIntent);
+                finish();
+
+            }
+
+            @Override
+            public void onError(Status status) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        if (mLocationPermissionGranted) {
+            getDeviceLocation();
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            mMap.setMyLocationEnabled(true); //to get blue marker with GPS icon
+            mMap.getUiSettings().setMyLocationButtonEnabled(false); //to hide GPS icon
+
+        }
+
+
+    }
+
+    private void initMAP() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(MapActivity.this);
+    }
+
+    private void getDeviceLocation() {
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        try {
+            if (mLocationPermissionGranted) {
+                final Task location = mFusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()) {
+                            Location current_location = (Location) task.getResult();
+                            moveCamera(new LatLng(current_location.getLatitude(), current_location.getLongitude()), DEFAULT_ZOOM);
+                        } else {
+                            Toast.makeText(MapActivity.this, "unable to get current location", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e) {
+            Log.e("error", e.getMessage());
+
+        }
+    }
+
+    private void moveCamera(LatLng latLng, float zoom) {
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+    }
+
+    private void getLocationPermission() {
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                mLocationPermissionGranted = true;
+                initMAP();
+            } else {
+                ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        } else {
+            ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0) {
+                    for (int i = 0; i < grantResults.length; i++) {
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                            mLocationPermissionGranted = false;
+                            return;
+                        }
+                    }
+                    mLocationPermissionGranted = true;
+                    //initialize our map
+                    initMAP();
+                }
+            }
+
+        }
+    }
+
 }
--- app/src/main/java/com/smm/sapp/sproject/Fragments/ProjectDetailsInterFragment.java
+++ app/src/main/java/com/smm/sapp/sproject/Fragments/ProjectDetailsInterFragment.java
@@ -1,6 +1,9 @@
 package com.smm.sapp.sproject.Fragments;
 
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -12,7 +15,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.smm.sapp.sproject.Activities.MapActivity;
 import com.smm.sapp.sproject.MyRequest;
 import com.smm.sapp.sproject.OkHttpCallback;
 import com.smm.sapp.sproject.R;
@@ -40,11 +47,16 @@ public class ProjectDetailsInterFragment extends Fragment {
     private EditText mArea2;
     private ImageView mUploadLikeImage;
     private EditText mCity;
-    private EditText mMap;
+    private TextView mMap;
     private EditText mBalance;
     private EditText mProjectDetailes;
     private TextView mAttachmentIn;
     private Button mSendIn;
+
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+    private static final int REQUEST_CODE = 1;
+    String s_lat, s_lng;
+
 
     public ProjectDetailsInterFragment() {
 
@@ -63,7 +75,11 @@ public class ProjectDetailsInterFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         Calligrapher calligrapher = new Calligrapher(getContext());
         calligrapher.setFont(getActivity(), "JFFlatregular.ttf", true);
-        initView();
+
+        if (isServicesOk()) {
+            initView();
+        }
+
         mSendIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -71,6 +87,22 @@ public class ProjectDetailsInterFragment extends Fragment {
             }
         });
 
+    }
+
+    public boolean isServicesOk() {
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(getActivity());
+        if (available == ConnectionResult.SUCCESS) {
+            //every thing is fine and the user can make map request
+            return true;
+
+        } else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
+            //error occur but we can resolve it
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(getActivity(), available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        } else {
+            Toast.makeText(getActivity(), "you can't make map request", Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 
     private void sendInterDesignRequest() {
@@ -83,8 +115,8 @@ public class ProjectDetailsInterFragment extends Fragment {
         map.put("colors", mDesignColor.getText().toString());
         map.put("city", mCity.getText().toString());
         map.put("area", mArea2.getText().toString());
-        map.put("lng", "");
-        map.put("lat", "");
+        map.put("lng", s_lng);
+        map.put("lat", s_lat);
         map.put("balance", mBalance.getText().toString());
         map.put("descr", mProjectDetailes.getText().toString());
 
@@ -115,7 +147,35 @@ public class ProjectDetailsInterFragment extends Fragment {
         mProjectDetailes = getView().findViewById(R.id.project_detailes);
         mAttachmentIn = getView().findViewById(R.id.attachment_in);
         mSendIn = getView().findViewById(R.id.send_in);
+
+        mMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), MapActivity.class);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                double lng = data.getDoubleExtra("lng", 0);
+                double lat = data.getDoubleExtra("lat", 0);
+
+                s_lat = String.valueOf(lat);
+                s_lng = String.valueOf(lng);
+
+                mMap.setText("خط الطول = " + s_lng + "\n" + "خط العرض = " + s_lat);
+                Log.d("lat", s_lat);
+
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+                Toast.makeText(getActivity(), "no data moved", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
 }
