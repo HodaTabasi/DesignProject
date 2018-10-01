42fe57e3ac6e532dc9213654fb00f3cbb64520db
MaryamAzz <maryam.ma@hotmail.com>
2018-10-01 21:37:14 +0300

    about

--- app/src/main/java/com/smm/sapp/sproject/ConstantInterFace.java
+++ app/src/main/java/com/smm/sapp/sproject/ConstantInterFace.java
@@ -9,4 +9,13 @@ public class ConstantInterFace {
     public static Boolean IS_REGISTER = false;
     public static TextView tv_msgs, tv_projects, tv_home, tv_portfolio, tv_profile;
     public static Boolean IS_USER_FAVORITE = false;
+
+
+    public static Boolean IS_ABOUT_OPENED = false;
+    public static Boolean IS_QUES_OPENED = false;
+    public static Boolean IS_RIGHTS_OPENED = false;
+    public static Boolean IS_CONDITIONS_OPENED = false;
+    public static Boolean IS_CALLUS_OPENED = false;
+
+
 }
