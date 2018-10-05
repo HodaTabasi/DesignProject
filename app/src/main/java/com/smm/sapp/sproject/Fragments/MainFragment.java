a62fcc6fb9d5327765b6977cc2275ceb02383f47
htabasi95@gmail.com <you@example.com>
2018-10-05 21:27:51 +0300

    some change in editproposal and mainFragment and viewProjectFragment

--- app/src/main/java/com/smm/sapp/sproject/Fragments/MainFragment.java
+++ app/src/main/java/com/smm/sapp/sproject/Fragments/MainFragment.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.smm.sapp.sproject.Activities.RegistrationActivity;
