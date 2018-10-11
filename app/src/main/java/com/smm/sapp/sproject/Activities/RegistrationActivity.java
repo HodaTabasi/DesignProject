760cc75d4b04211fa40baae5b154626774beecb3
htabasi95@gmail.com <you@example.com>
2018-10-11 18:06:42 +0300

    some change

--- app/src/main/java/com/smm/sapp/sproject/Activities/RegistrationActivity.java
+++ app/src/main/java/com/smm/sapp/sproject/Activities/RegistrationActivity.java
@@ -4,6 +4,9 @@ import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.smm.sapp.sproject.Fragments.RegisterFragment;
 import com.smm.sapp.sproject.HelperClass.FragmentsUtil;
@@ -12,14 +15,29 @@ import com.smm.sapp.sproject.R;
 import me.anwarshahriar.calligrapher.Calligrapher;
 
 public class RegistrationActivity extends AppCompatActivity {
+    ImageView ic_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
+        ic_back = findViewById(R.id.ic_back);
+
         FragmentsUtil.addFragment(RegistrationActivity.this, R.id.register_container, new RegisterFragment(), false);
         Calligrapher calligrapher = new Calligrapher(this);
         calligrapher.setFont(this, "JFFlatregular.ttf", true);
+
+        ic_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
