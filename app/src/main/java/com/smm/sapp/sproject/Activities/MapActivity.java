9590496fec0068c340d35f4f077de4328fc26a22
Mohamed Assem <mo7mad.assim@gmail.com>
2018-11-09 01:16:46 +0200

    Map updates commit

--- app/src/main/java/com/smm/sapp/sproject/Activities/MapActivity.java
+++ app/src/main/java/com/smm/sapp/sproject/Activities/MapActivity.java
@@ -1,19 +1,26 @@
 package com.smm.sapp.sproject.Activities;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.location.Location;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -28,11 +35,15 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.smm.sapp.sproject.R;
+
+import java.util.Objects;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -42,6 +53,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
     private GoogleMap mMap;
     private FusedLocationProviderClient mFusedLocationProviderClient;
+    private Marker marker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -95,9 +107,71 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
             mMap.setMyLocationEnabled(true); //to get blue marker with GPS icon
             mMap.getUiSettings().setMyLocationButtonEnabled(false); //to hide GPS icon
 
+            // Assem
+            googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                @Override
+                public void onMapClick(LatLng latLng) {
+                    if (marker != null) { //if marker exists (not null or whatever)
+                        marker.setPosition(latLng);
+                    } else {
+                        marker = mMap.addMarker(new MarkerOptions()
+                                .position(latLng)
+                                .title("test")
+                                .draggable(true));
+                        setupAddBookmarkDialog(latLng);
+                    }
+                }
+            });
         }
+    }
 
+    // Assem
+    private void sendDataBack(LatLng latLng) {
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra("lng", latLng.longitude);
+        returnIntent.putExtra("lat", latLng.latitude);
+        setResult(Activity.RESULT_OK, returnIntent);
+        finish();
+    }
 
+    // Assem
+    private void setupAddBookmarkDialog(final LatLng latLng) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        @SuppressLint("InflateParams") final View mView = LayoutInflater.from(this).inflate(R.layout.dialog_add_project_place, null);
+        Button addBtn = mView.findViewById(R.id.dialog_add_bookmark_add_btn);
+        Button cancelBtn = mView.findViewById(R.id.dialog_add_bookmark_cancel_btn);
+
+        builder.setView(mView);
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendDataBack(latLng);
+                alertDialog.dismiss();
+            }
+        });
+
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+            }
+        });
+    }
+
+    // Assem
+    public void customMarker(Context context, GoogleMap mMap, int drawable, String title, LatLng latLng) {
+        int height = 80;
+        int width = 80;
+        BitmapDrawable bitmapDraw = (BitmapDrawable) context.getResources().getDrawable(drawable);
+        Bitmap b = bitmapDraw.getBitmap();
+        Bitmap smallMarker = Bitmap.createScaledBitmap(b, width, height, false);
+        MarkerOptions mMarkerOptions = new MarkerOptions();
+        mMarkerOptions.title(title).position(latLng)
+                .icon(BitmapDescriptorFactory.fromBitmap(smallMarker));
+        mMap.addMarker(mMarkerOptions);
     }
 
     private void initMAP() {
@@ -109,7 +183,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         try {
             if (mLocationPermissionGranted) {
-                Log.e("rrrrrr","rrrr");
+                Log.e("rrrrrr", "rrrr");
                 final Task location = mFusedLocationProviderClient.getLastLocation();
                 location.addOnCompleteListener(new OnCompleteListener() {
                     @Override
@@ -120,19 +194,18 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                             try {
                                 moveCamera(new LatLng(current_location.getLatitude(), current_location.getLongitude()), DEFAULT_ZOOM);
 
-                            }catch (Exception e){
+                            } catch (Exception e) {
                                 onBackPressed();
                                 startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
                                 Toast.makeText(MapActivity.this, "الرجاء قم بإعطاء الصلاحيات للموقع", Toast.LENGTH_SHORT).show();
                             }
                         } else {
                             Toast.makeText(MapActivity.this, "unable to get current location", Toast.LENGTH_SHORT).show();
-
                         }
                     }
                 });
-            }else {
-                Log.e("rrrrrr","ffff");
+            } else {
+                Log.e("rrrrrr", "ffff");
 
             }
         } catch (SecurityException e) {
